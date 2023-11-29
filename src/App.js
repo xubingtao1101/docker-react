4875602645e496a3cbb37b57607ba5a707731491
徐冰涛 <xubingtao@xubingtaodeMacBook-Pro.local>
2023-11-29 15:00:51 +0800

    init commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. hhhhhhhh12
         </p>
         <a
           className="App-link"
@@ -16,6 +16,14 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+        </a>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Vue
         </a>
       </header>
     </div>
